e4d9ce3642a94c54e04a5632344bffdfe36df9c6
mAlturki42 <102689668+mAlturki42@users.noreply.github.com>
2023-08-04 14:32:29 +0100

    Update TestBooks.java

--- TestBooks.java
+++ TestBooks.java
@@ -1,4 +1,46 @@
 
+Caused by: org.hibernate.AnnotationException: No identifier specified for entity: com.db.grad.javaapi.model.Bookuser
+
+
+package com.db.grad.javaapi.model;
+import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "book_user")
+public class Bookuser {
+
+    @ManyToMany
+    @JoinTable(name= "book", joinColumns = @JoinColumn(name="book_id", referencedColumnName = "id"))
+    private int book_id;
+    @ManyToMany
+    @JoinTable(name= "user", joinColumns = @JoinColumn(name="user_id", referencedColumnName = "id"))
+    private int user_id;
+
+    @Column(name= "book_id", nullable = false)
+    public int getBook_id() {
+        return book_id;
+    }
+
+    public void setBook_id(int book_id) {
+        this.book_id = book_id;
+    }
+
+    @Column(name= "user_id", nullable = false)
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
+
+
+
 ***************************
 APPLICATION FAILED TO START
 ***************************
