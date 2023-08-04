c59a7531953cd85a81e9f915eb66909bd84a0fc0
mAlturki42 <102689668+mAlturki42@users.noreply.github.com>
2023-08-04 20:12:15 +0100

    Update Book.java

--- Book.java
+++ Book.java
@@ -75,4 +75,33 @@ public class TradesHandler implements ITradesService
     }
 }
 
-ITradesService
+ITradesService:
+
+package com.db.grad.javaapi.service;
+
+import com.db.grad.javaapi.model.Trades;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface ITradesService
+{
+    public List<Trades> getAllTrades();
+    //public List<Trades> getTradeByName(String TradeName );
+
+    /*
+    public Dog addDog(Dog theDog);
+
+    public long getNoOfDogs();
+
+    public boolean removeDog(long uniqueId);
+
+    public Dog getDogById(long uniqueId);
+
+    public Dog getDogByName(String dogsName );
+
+    public Dog updateDogDetails(Dog dogToUpdate);
+    */
+}
+
